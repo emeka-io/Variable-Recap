a7eff8814f3b45b7bcb7aa2dfca86a9cc959092b
Emeka <theemekamail@gmail.com>
2025-11-01 12:53:44 -0700

    Variables in Python
    A light recap of variables in Python.

---
+++ recap.py
@@ -0,0 +1,27 @@
+#Defining variables
+name = 'John Smith'
+age = 20 
+new_patient = True
+print(type(age))
+
+#Getting input
+first_name = input("What's your name? ")
+fav_colour = input("What's your favorite color? ")
+print(f"{first_name} likes {fav_colour}")
+
+#Type conversion
+weight_gram = int(input("What's your weight in grams? "))
+weight_kg = weight_gram / 1000
+print(f"You weigh {weight_kg} KG.")
+
+#Strings
+# to define strings to contain more than one line we use triple quotes.
+mail = '''
+Hi John,
+
+Here is our first email to you.
+
+Thank you,
+The Emecron Team.
+
+'''
